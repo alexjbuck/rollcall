decf7c386de0e62eb1d2bca9c723c5521533a9cc
Alexander Buck <alexjbuck@gmail.com>
2025-08-29 20:03:27 -0400

    Updated to workspace structure

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: path.dirname(fileURLToPath(import.meta.url))
+  }
 };
 
 export default nextConfig;
